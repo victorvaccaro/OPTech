73c23b4a71a800be95cd19439e2562939d9fe7df
Victor <victo.vaccaro@gmail.com>
2016-03-30 00:12:51 +0200

    finalisation de equipe

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
-$(".pointeur").click(function(){
-    $(this).parent().parent().next().next().next().toggle();
-});
+var img,nom,description,num;
+function setNumero(numero) {
+	switch(numero){
+		case 1:
+			img="http://placemi.com/petnp/600x700";
+	    	nom="Marc Marcinkowski";
+	    	description="Jusqu’à maintenant, 3 sociétés m’ont fait confiance. Premièrement Auchan pour mon stage de 1ère année pour la découverte de l’entreprise. Ensuite, j’ai intégré la DSI en 2ème année chez Arvato pour développer une application web pour sensibiliser leur employé de toute la France à la sécurité de l’information, entre autre sur internet. Enfin, j’ai eu la chance de faire mon stage d’apprenant ingénieur de 6 mois chez Devos Vandenhove. J’y ai conçu, réalisé et développer un banc d’essai dans l’optique d’aider les techniciens à réparer des variateurs Siemens.";
+	   		break;
+	   	case 2:
+			img="http://placemi.com/xruzn/600x700";
+	    	nom="Quentin Delrue";
+	    	description="À la fin de ma première année de BTS IRIS (Informatique et Réseaux des Industries et Services Techniques) j’ai passé 3 mois de stage au sein de GV Diffusion un entreprise de vente de pièces détachées et accessoires pour chauffage, bâtiment et fumisterie. J’ai par la suite réalisé mon stage de 4ème année de la formation IG2I de Centrale Lille dans l’usine Délifrance de Dunkerque. Ce stage avait pour but de remanier la base de données d’un atelier de production de pain surgelé précuit dans le but de réduire le temps d’impression des fiches de production quotidiennes et simplifier la modification des données produit.";
+	    	break;
+    	case 3:
+	    	img="http://placemi.com/wctme/600x700";
+	    	nom="Amine Bounif";
+	    	description="Durant ces 4 années au sein de l’Ecole Centrale de Lille formation IG2I, j’ai pu acquérir une grande expérience dans le monde de l’entreprise. En effet durant la première année,j’ai réalisé mon stage de découverte d’entreprise chez Orpéa à Bruxelles pendant 5 semaines. En deuxième année j’ai intégré une grande société, l’Office Chérifien des Phosphates pour un stage d’une durée de 3 mois en tant que technicien. J’ai pu perfectionner mon anglais grâce à un stage de 3 mois en Angleterre à Sheffield, j’ai été chargé de la création d’un site web. Et finalement pour mon stage L4, j’étais assistant ingénieur au sein d’une Start Up, Huguet It Eirl basé à Tourcoing.";
+	    	break;
+    	case 4:
+			img="http://placemi.com/qwspg/600x700";
+	    	nom="Victor Vaccaro";
+	    	description="Mon parcours se différencie des autres grâce à deux stages effectués à l’international. J’ai pu effectuer mon premier en Italie dans le domaine de la mécatronique, pendant 3 mois afin de conclure mon DUT informatique, puis je suis parti en école préparatoire pendant an afin d'intégrer l’IG2I. Lors du stage de 3e et 4e année, je suis parti à Miami pendant 8 mois pour travailler dans le département informatique de Neptune Cigar, l’un des plus gros revendeurs de cigares en ligne du monde. J’ai pu progresser dans le développement d’applications Android/IOS, la maintenance et l’amélioration de plusieurs sites web dont le site d’e-commerce, ainsi que le développement d’applications web. J’ai grâce à cette opportunité eu l’occasion de découvrir une autre culture du travail en entreprise et ai pu améliorer considérablement mes compétences de travail en groupe, le tout dans une autre langue.";
+			break;
+		case 5:
+			img="img/valentin.JPG";
+	    	nom="Valentin Pétré";
+	    	description="Au cours de ma première année de DUT GEII, j’ai effectué un stage chez Sipa Menuiserie. Ce dernier m’aura permis de découvrir le milieu de l’entreprise aussi bien par ses bureaux que par son atelier de fabrication. J’ai également pu travailler sur divers automates. Mon stage de fin d’études du DUT GEII fût un stage Erasmus de trois mois, qui s’est déroulé au sein du département Mécatronique de l’École Polytechnique de Varsovie (Pologne). J’ai ainsi pu y développer en C++ ’un logiciel de reconnaissance faciale à l’aide d’OpenCV. Mon stage de quatrième année s’est quant à lui effectué dans l’usine Renault Douai, au sein du département Emboutissage. Durant ce stage de six mois, j’ai eu à gérer le pilotage de l’automatisation et de la robotisation d’un site de formation. J’ai également pu intervenir sur différentes chaînes de fabrication. Au cours de la troisième année de ma formation Centrale Lille IG2I, j’ai effectué un stage linguistique dans une ferme à proximité d’Ottawa (Canada).";
+			break;
+		case 6:
+			img="http://placemi.com/rxco7/600x700";
+	    	nom="Yahya Arba";
+	    	description="Au cours de mon cursus au sein de la formation IG2I de Centrale de Lille, j’ai effectué 3 stages durant lesquels on m’a fait confiance. En première année j’ai effectué mon stage chez BMW Brienne-auto en tant qu’assistant magasinier dans le cadre d’un stage ouvrier / découverte d’entreprise. En deuxième année un stage développeur de plugins pour Sage CRM fut réalisé chez ATEJA á Lille. Pour le stage de quatrième année, la société ATEJA m’a fait confiance et m’a attribué la responsabilité de piloter les projets de plugins CRM. J’ai aussi eu l’opportunité de parfaire mon anglais durant un stage linguistique de 3 mois á Malte.";
+			break;
+		case 7:
+			img="http://placemi.com/kzsjt/600x700";
+	    	nom="Jérémie Pannequin";
+	    	description="L'opportunité de prendre part à des projets industrielles s'est présentée plusieurs fois au cours de ma formation. Ainsi j'ai acquis une expérience de travail relativement riche et internationnalle au travers de différents stages. Pendant 3 mois j'ai pris part à un projet informatique concernant l'analyse d'image par ordinateur au sein de l'université polytechnique de Varsovie. Un stage d'assistant ingénieur au sein de la société ERTP Hibon à été ma plus longue expérience professionnelle. Elle m'a permise de prendre part et de suivre l'évolution d'un projet complexe, où j'ai notamment réalisé l'automatisation d'un groupe de vide industrielle. (cf http://ertphibon.wix.com/hibox) Finalement un stage de 3 mois en Nouvelle Zélande m'a donné l’opportunité de découvrir le monde du marketing au travers de la vente de produit décoratif, et ainsi améliorer mes qualités linguistiques et relationnelles.";
+	    	break;
+	    case 8:
+	    	img="http://placemi.com/m2qx7/600x700";
+    		nom="Jordan Bonhommet";
+    		description="Je m’appelle Jordan Bonhommet, j’ai 23 ans. Je suis arrivé à Centrale Lille formation IG2I en 2014, directement en 3ème année après 2 ans de classe préparatoire TSI. J’ai fait un bac STI Génie Électrotechnique. Je suis actuellement en 4ème année, avec comme option GSA (Génie des Système Avancé), informatique industriel. Après l’obtention de mon diplôme, j’aimerais travailler dans un pôle de R&D dans l’industrie aéronautique ou automobile.";
+    		break;
+    }
+    num=numero;
+    $("#imgPersonne").attr("src",img);
+    $("#nomPersonne").html(nom+"<button type='button' class='close' data-dismiss='modal'>&times;</button><hr>");
+    $("#descriptionPersonne").html(description);
+}
+function fleche(sens) {
+	if (sens==="gauche"){
+		if (num===1) num=8;
+		else num--;
+	} else if (sens==="droite"){
+		if (num===8) num=1;
+		else num++;	}
+	setNumero(num);
+}
